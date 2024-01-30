31282831084ca6e29679dc0fe72e172e04aaf132
Tyler Strunk <wtstrunk1@gmail.com>
2024-01-30 16:38:06 -0500

    After considerable refactoring I have working code again. The calculator works flawlessly from what I have tested (and coded) for. I still need to activate the last buttons, but after that I should be nearly finished. Some things I changed: 1.)Split my updateDisplay into two separate functions (updateDisplay & setNumbers) 2.) Changed large if else statements and dedicated operation functions into a much more simple switch statement. 3.) Created the setOperator function that is called when an operator button is clicked. Instead of directly setting the operator to the object it calls the setOperator function, which not only sets the operator, but if one is previously set, performs the calculation (operate function).

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 
-//Our math operations
-const add = (a, b) => parseFloat(a) + parseFloat(b); 
-const subtract = (a, b) => parseFloat(a) - parseFloat(b);
-const multiply = (a, b) => parseFloat(a) * parseFloat(b);
-function divideNumbers(a, b) {
-    if (b === 0) {
-        return "Error: Division by zero is not allowed";
-    }
-    return parseFloat(a) / parseFloat(b);
-}
-
-//Adding solutionTracker to keep updated calculation. 
-//This is more to free up num1 & num2 for all use cases. 
 let calculation = {
     num1: "",
     operator: "",
@@ -19,114 +6,181 @@ let calculation = {
     solutionTracker: ""
 }
 
-//THIS SECTION HANDLES THE NUMBER BUTTONS
 let addClick = document.querySelectorAll(".number");
-
 addClick.forEach(number => {
     number.addEventListener('click', () => {
-        updateCalculation(number.textContent)
+        setNumbers(number.textContent)
     }); 
 });
 
-function updateCalculation(buttonValue){
-    let display = document.getElementById('display');
-    display.textContent = buttonValue;
-
-
-
-    if(calculation.operator === ""){  
-            calculation.num1 += buttonValue;
-            display.textContent = calculation.num1;
-    }
-    else {
+function setNumbers(buttonValue){
+    if(calculation.operator === ""){
+        calculation.num1 += buttonValue;
+        // display.textContent = calculation.num1;
+        // console.log(calculation.num1);
+    } 
+    else{
         calculation.num2 += buttonValue;
+        // display.textContent = calculation.num2;
+        // console.log(calculation.num2);
+    }
+    updateDisplay();
+}
+
+function updateDisplay() {
+    // if(calculation.solutionTracker !== ""){
+    //     console.log("here1");
+    //     display.textContent = calculation.solutionTracker;
+    // }
+    if(calculation.num2 !== ""){
+        console.log("here2");
         display.textContent = calculation.num2;
-        
-    };
-};
-//NUMBER BUTTON END 
+    }
+    else{
+        console.log("here3");
+    display.textContent = calculation.num1;
+    }
+  }
+
+//Function sets calculation.operator
+function setOperator(operator){
+    if(calculation.operator !== ""){
+        operate(); 
+    }
+    calculation.operator = operator;
+    //updateDisplay();
+}
 
 //OPERATOR BUTTONS
 let multiplication = document.getElementById('multiply');
 multiplication.addEventListener('click', () => {
-    calculation.operator = "*";
+    setOperator("*");
 });
 
 let division = document.getElementById('divide');
 division.addEventListener('click', () => {
-    calculation.operator = "/";
+    setOperator("/");
 });
 
 let addition = document.getElementById('addition');
 addition.addEventListener('click', () => {
-    calculation.operator = "+";
+    setOperator("+")
 });
 
 let subtraction = document.getElementById('subtraction');
 subtraction.addEventListener('click', () => {
-    calculation.operator = "-";
+    setOperator("-")
 });
 //END OPERATOR BUTTONS
 
-
-let findSolution = document.getElementById('equals');
-findSolution.addEventListener('click', () => {
-    let display = document.getElementById('display');
-    let solution = operate(calculation);
-    display.textContent = solution;
-    
-    calculation.solutionTracker = solution;
-    console.log(calculation.solutionTracker); 
-    calculation.num1 = "";
-    calculation.num2 = "";
-    // console.log(calculation.num2)
-    calculation.operator = "";
-    // console.log(calculation.num1);
+//Equals Sign Button -----------------------------------
+let equalsSign = document.getElementById('equals');
+equalsSign.addEventListener('click', () => {
+    operate(calculation);
 })
-
-
-
+//------------------------------------------------------
 
 function operate(){
-    if (calculation.operator === "*"){
-         if(calculation.solutionTracker === ""){
-            solution = multiply(calculation.num1, calculation.num2);
-            return solution;
-        }
-        else{
-            solution = multiply(calculation.solutionTracker, calculation.num2)
-            return solution; 
-        }
+    const num1 = parseFloat(calculation.num1);
+    const num2 = parseFloat(calculation.num2);
+
+    switch (calculation.operator) {
+      case "+":
+        calculation.solutionTracker = num1 + num2;
+        break;
+      case "-":
+        calculation.solutionTracker = num1 - num2;
+        break;
+      case "*":
+        calculation.solutionTracker = num1 * num2;
+        break;
+      case "/":
+        calculation.solutionTracker = num1 / num2;
+        break;
+      default:
+        return;
     }
-    else if(calculation.operator === "/"){
-        if(calculation.solutionTracker === ""){
-            solution = divideNumbers(calculation.num1, calculation.num2);
-            return solution;
-        }
-        else{
-            solution = divideNumbers(calculation.solutionTracker, calculation.num2)
-            return solution;
-        }
-    }
-    else if(calculation.operator === "+"){
-        if(calculation.solutionTracker === ""){
-            solution = add(calculation.num1, calculation.num2);
-            return solution; 
-        }
-        else {
-            solution = add(calculation.solutionTracker, calculation.num2);
-            return solution;
-        }
-    }
-    else if(calculation.operator === "-"){
-        if(calculation.solutionTracker === ""){
-            solution = subtract(calculation.num1, calculation.num2);
-            return solution;
-        }
-        else {
-            solution = subtract(calculation.solutionTracker, calculation.num2);
-            return solution; 
-        }
-    }
-    
+    calculation.num1 = calculation.solutionTracker;
+    calculation.num2 = "";
+    calculation.operator = "";
+    updateDisplay(); 
 }
+
+
+
+
+//I need this program to work without the use of the equals sign button. 
+//That means I need to be ablet of ind the solution just using the number and operator buttons. 
+//When do I call findSolution? How do I update the object and use the solutionTracker element to do this? 
+//Currently 1.) Hit number button -> this updates the display and stores number in calculation
+//2.) Hit operator button -> This stores the operator in calculation
+//3.) Hit number buttons -> Update display ad stores number2 in calculation
+//4.) Hit operator button -> This stores operator in calculation 
+//                --Need this to 1.) Find solution based on calculation & display it when the operator button is clicked a second time
+//                               2.) I think it needs to store the solution in calculation.solutionTracker & empty num1/num2
+//                               3.) Then solutionTracker = num1 when finding the second solution? 
+
+// function findSolution(){
+//     let display = document.getElementById('display');
+//     let solution = operate(calculation);
+//     display.textContent = solution;
+
+//     calculation.solutionTracker = solution;
+//     console.log(calculation.solutionTracker); 
+//     calculation.num1 = "";
+//     calculation.num2 = "";
+    // console.log(calculation.num2)
+    //calculation.operator = "";
+    // console.log(calculation.num1);
+// }
+
+
+
+
+
+     // updateDisplay();
+//     if (calculation.operator === "*"){
+//          if(calculation.solutionTracker === ""){
+//             solution = multiply(calculation.num1, calculation.num2);
+
+//             return solution; 
+//         }
+//         else{
+//             solution = multiply(calculation.solutionTracker, calculation.num2)
+//             return findSolution(solution); 
+//         }
+//     }
+// }
+    
+//     else if(calculation.operator === "/"){
+//         if(calculation.solutionTracker === ""){
+//             solution = divideNumbers(calculation.num1, calculation.num2);
+//             return solution;
+//         }
+//         else{
+//             solution = divideNumbers(calculation.solutionTracker, calculation.num2)
+//             return solution;
+//         }
+//     }
+//     else if(calculation.operator === "+"){
+//         if(calculation.solutionTracker === ""){
+//             solution = add(calculation.num1, calculation.num2);
+//             return solution; 
+//         }
+//         else {
+//             solution = add(calculation.solutionTracker, calculation.num2);
+//             return solution;
+//         }
+//     }
+//     else if(calculation.operator === "-"){
+//         if(calculation.solutionTracker === ""){
+//             solution = subtract(calculation.num1, calculation.num2);
+//             return solution;
+//         }
+//         else {
+//             solution = subtract(calculation.solutionTracker, calculation.num2);
+//             return solution; 
+//         }
+//     }
+    
+
